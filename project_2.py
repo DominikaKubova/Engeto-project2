5ede307527bd5689e54f38ac15d4b95235704cdf
Dominika Kubová <dominika.kubova@gmail.com>
2024-03-02 18:02:44 +0100

    added functions and corrections.

--- project_2.py
+++ project_2.py
@@ -13,10 +13,84 @@ import sys
 
 SEPARATOR = 30 * "="
 
-def bulls_cows_count(guess, number):
+def introduction():
+    """Prints a welcome massage and informs the palyer that the random number is generated"""
+    print(SEPARATOR)
+    print("Welcome to the game - Bulls & Cows!")
+    print(SEPARATOR)
+    print("I have generated a random 4-digit number for you.\nLet's play!.")    
+
+def random_number():
+    """Generates random 4 digit number with no duplicities"""
+    number = str(random.randint(1000 , 9999))
+    while len(set(number)) != 4: 
+    #print(number)
+        number = str(random.randint(1000,9999))
+    return(number)     
+
+def hint(number: str, guessed_numbers: list):
+    """
+    Every 5 valid guesses player is asked if wants to contitnue or the randon number is reveled.
+
+    Keyword arguments:
+    guess -- input from the player
+    guessed_numbers -- list of already used valid guesses
+    """
+    if len(guessed_numbers) % 5 == 0 and len(guessed_numbers) != 0:
+        message_hint = "Do you want to reveal guessed number?\n"\
+                        "If yes, answer \"YES\","\
+                        " if you want to keep trying just hit enter!"
+        decision = input(message_hint)
+        if decision == "YES":
+            print(number)
+            sys.exit()
+        else:
+            print("OK, keep trying!")
+
+def player_guess_check(guess: str):
+    """
+    Checks the input from the player. If the format is not correct, message assigned to the problem is shown.
+    If the format is correct game continues.
+
+    Keyword arguments:
+    guess -- input from the player
+    """
+    guess_list = [i for i in guess]
+    if guess_list[0] == "0":
+        print("The 4-digit number can not start with 0.")
+    if not guess.isdigit(): 
+        print("The inserted guess is not in a valid format!")
+    if len(guess) != 4 and guess.isdigit():
+        digits_of_guess = len(guess)
+        print("The number has to contain 4-digits. Your guess conatins", digits_of_guess, "digits.")
+
+def player_guess_valid(number: str, guess: str, guessed_numbers: list):
+    """If player's guess is in valid format, check if the guess has already been tried,
+    if not, adds the guess to list of guesses
+    
+    Keyword arguments:
+    number -- random number generated
+    guess -- input from the player
+    guessed_numbers -- list of already used valid guesses
+    """
+    if guess.isnumeric() and int(guess) in range(1000 , 9999):
+        if guess not in guessed_numbers:
+            bulls_cows_count(guess, number)
+            guessed_numbers.append(guess)
+        else:
+            print("You have already tried this number.")
+     
+def bulls_cows_count(guess: str, number: str):
+    """
+    Counting bulls and cows from guessed number.
+
+    Keyword arguments:
+    guess -- input from the player
+    number -- random number generated
+    """
     bull_cow = [0,0] 
-    guess_list = [i for i in str(guess)]
-    number_list = [i for i in str(number)]
+    guess_list = [i for i in guess] 
+    number_list = [i for i in number]
     
     for digit_guess, digit_number in zip(guess_list, number_list):
         if digit_guess in number_list:
@@ -26,47 +100,28 @@ def bulls_cows_count(guess, number):
                 bull_cow[1] += 1
     print("BULLS = ", bull_cow[0], "/ COWS = ", bull_cow[1])    
     
+def game():
+    """
+    Process of the game: introtuction of the play, generating random number, validating 
+    the player's guess with bulls and cows counter until the guess is equal to number.
+    """
+    introduction()
+    number = random_number()
+    print(SEPARATOR)
+    #print(type(number))
 
+    guessed_numbers = []
+    guess = 0
+    #guess = None
+
+    while guess != number: 
+        hint(number, guessed_numbers)    
+        guess = input("Enter a 4-digit number: ")
+        player_guess_check( guess)
+        player_guess_valid(number, guess, guessed_numbers)
+        
+    print("Congratulations, the number", guess, "is the correct guess! You won in", len(guessed_numbers), "attempts.")
 
 ## MAIN CODE ##
-print(SEPARATOR)
-print("Welcome to the game - Bulls & Cows!")
-print(SEPARATOR)
-print("I have generated a random 4-digit number for you.\nLet's play!.")
-
-#Generation of random 4-digit number which does not start with 0
-number = str(random.randint(1000 , 9999))
-#print(number)
-
-print(SEPARATOR)
-
-
-
-#Program will notify if the quessed number is not 4-digit long, will be duplicated, starting with 0 or will not be integer
-
-guessed_numbers = []
-guess = 0
-
-while guess != number:   
-    if len(guessed_numbers) % 5 == 0 and guess != 0:
-        decision = input("Do you want to reveal guessed number? If yes, answer \"YES\", if you want to keep trying just hit enter!")
-        if decision == "YES":
-            print(number)
-            sys.exit()
-        else:
-            print("OK, keep trying!") 
-
-    guess = input("Enter a 4-digit number: ")
-    if guess.isnumeric() and int(guess) in range(1000 , 9999):
-        if guess not in guessed_numbers:
-            guess_analyses = bulls_cows_count(guess, number)
-            guessed_numbers.append(guess)
-        else:
-            print("You have already tried this number.")
-    else:
-        print("The inserted guess is not in a valid format! Please check if you used 4-digit number.")
-        continue
-
-
-
-print("Congratulations, the number", guess, "is the correct guess! You won in", len(guessed_numbers), "attempts.")
+if __name__ == "__main__":
+    game()
